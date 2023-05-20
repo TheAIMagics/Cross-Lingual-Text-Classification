20b2170814a3d596a4162a87d34fb4eff4c45822
satyazmx <thakursatya6@gmail.com>
2023-05-20 09:23:54 +0530

    add basic project setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="iNeuron",
+    description="Sentiment Analysis",
+    author_email="thakursatya6@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
